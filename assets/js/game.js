bc4edfe8d109c446fdd83ee9a6d7343a94ee746c
dshaw1242 <david.shaw1242@gmail.com>
2021-03-21 18:14:23 -0600

    Setup first round of battle in game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,42 @@
-window.alert("This is an alert! Javascript is running");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// logging multiple values all at once like below:
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // alert player that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+    // subtract player attack from enemy health
+    enemyHealth = enemyHealth - playerAttack;
+    // Log resulting message to the console to know that it worked
+    console.log(enemyName + " has " + enemyHealth + " health left");
+
+    // Check enemy health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died! Good Fight!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // subtract playerHealth from enemyAttack value
+    playerHealth = playerHealth - enemyAttack;
+    // Log resulting message to the console to know that it worked
+    console.log(playerName + " has " + playerHealth + " health left");
+
+    // check player health
+    if (playerHealth <= 0){
+        window.alert(playerName + " has died! Game Over!");
+    }
+    else {
+        window.alert(playerName + " has " + playerHealth + " health left.");
+    }
+} ;
+
+fight();
